1a4dadea265b380821ed32678b097348e011580d
Matt Dean <mjdean1994@gmail.com>
2016-01-22 11:41:19 -0500

    Initial commit.

---
+++ DataDotNet/DataDotNetExtensions.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+using RazorEngine;
+
+namespace DataDotNet
+{
+    public static class DataDotNetExtensions
+    {
+        public static MvcHtmlString PieChart(this HtmlHelper helper)
+        {
+            var template = GetTemplate("DataDotNet.Views.Test.cshtml");
+
+            // parse template using RazorEngine
+            var html = RazorEngine.Razor.Parse(template);
+
+            return new MvcHtmlString(html);
+        }
+
+        /// <summary>
+        /// Read an embedded resource as a string
+        /// </summary>
+        private static string GetTemplate(string resourceName)
+        {
+            var assembly = System.Reflection.Assembly.GetExecutingAssembly();
+
+            using (var stream = assembly.GetManifestResourceStream(resourceName))
+            {
+                using (var reader = new System.IO.StreamReader(stream))
+                {
+                    string result = reader.ReadToEnd();
+                    return result;
+                }
+            }
+        }
+    }
+}
---
+++ TestProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TestProject.Startup))]
+namespace TestProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
